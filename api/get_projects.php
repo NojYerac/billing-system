8d84a301fe35d5c267bbc3ac0af9af95b20e79e6
Noj Yerac <noj.yerac@gmail.com>
2014-07-04 22:05:06 -0500

    added get_projects.php to api directiory

--- api/get_projects.php
+++ api/get_projects.php
@@ -14,8 +14,7 @@ if (!isset($_SESSION['user_priv']) ||
         array('Administrator', 'Employee')
     )) {
 	http_response_code(401);
-    echo 'Not Authorized';
-    exit();
+    die('Not Authorized');
 }
 
 $params = array();
@@ -37,25 +36,36 @@ foreach($_REQUEST as $key => $value) {
 		case 'customer_id':
 			assert(
 				is_string($value) &&
-				preg_match('/^[A-Za-z0-9]{24}$/', $value) &&
-				in_array($value, $_SESSION['visible_clients']),
+				preg_match('/^[A-Za-z0-9]{24}$/', $value),
 				'Bad customer id.'
+			);
+			$params['customer_id'] = $value;
+			break;
+		case 'project_id':
+			assert(
+				is_string($value) &&
+				preg_match('/^[A-Za-z0-9]{24}$/', $value),
+				'Bad project id.'
 			);
 			$params['_id'] = new MongoId($value);
 			break;
-		case 'customer_name':
+		case 'project_name':
 			assert(
 				is_string($value),
-				'Bad project id.'
+				'Bad project name.'
 			);
-			$params['customer_name'] = $value;
+			$params['project_name'] = $value;
+			break;
+		case 'active':
+			assert(in_array($value, array('0', '1')), 'Bad active value.');
+			$params['active'] = (bool)$value;
 			break;
 		default:
 			Die('Invalid parameter.');
 	}
 }
 
-$result = json_encode(get_all_documents('clients', $params));
+$result = json_encode(get_all_documents('projects', $params));
 header('Content-type: application/json');
 header('Content-length: ' . strlen($result));
 echo $result;
--- assert.php
+++ assert.php
@@ -2,7 +2,7 @@
 assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_QUIET_EVAL, 1);
 function assert_handler($file, $line, $code, $desc = null){
-	echo $desc?$desc:'';
+	die($desc?$desc:'');
 }
 assert_options(ASSERT_CALLBACK, 'assert_handler');
 ?>
